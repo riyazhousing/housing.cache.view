77575ce85855706104bdc8f953f90cd1acde4eb5
Mohammed Riyaz <riyazmd.pechu@gmail.com>
2017-07-07 02:24:31 +0530

    Added Template

--- app/controllers/housing_cache_view/api_controller.rb
+++ app/controllers/housing_cache_view/api_controller.rb
@@ -1,7 +1,14 @@
 module HousingCacheView
   class ApiController < ::HousingCacheView::ApplicationController
     def bla
-      render "housing_cache_view/cache_view_home", :status => 200
+      # http://www.java-samples.com/showtutorial.php?tutorialid=1090
+      # require "base64"
+
+      # enc   = Base64.encode64('Send reinforcements')
+      #                     # -> "U2VuZCByZWluZm9yY2VtZW50cw==\n"
+      # plain = Base64.decode64(enc)
+      #                     # -> "Send reinforcements"
+      render "housing_cache_view/home", :status => 200
     end
   end
 end
